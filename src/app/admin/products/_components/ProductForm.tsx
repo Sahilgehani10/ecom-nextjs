efea0ab097dcf1c50b46721cfc044736e317ed22
sahil gehani <sahilgehani47@gmail.com>
2024-06-03 22:34:26 +0530

    admin dash(in progress)

---
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { Label } from "@radix-ui/react-label"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import { formatCurrency } from "@/lib/formatters"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+import { addProduct } from "../../_actions/products"
+
+export function ProductForm(){
+    const [priceInCents,setPriceInCents]=useState<number>()
+    return <form action={addProduct} className="space-y-8">
+        <div className="space-y-2">
+            <Label htmlFor="name">Name</Label>
+            <Input type="text" name="name" required/>
+        </div>
+        <div className="space-y-2">
+            <Label htmlFor="priceInCents">Price in Cents</Label>
+            <Input type="text" name="priceInCents" id="priceInCents" required value={priceInCents} onChange={e=>setPriceInCents(Number(e.target.value)||undefined)}/>
+            <div className="text-muted-forground">
+                {formatCurrency((priceInCents||0)/100)}
+            </div>
+        </div>
+        <div className="space-y-2">
+            <Label htmlFor="description">Description</Label>
+            <Textarea id="description" name="description" required/>
+        </div>
+        <div className="space-y-2">
+            <Label htmlFor="file">File</Label>
+            <Input type="file" id="file" name="file" required/>
+        </div>
+        <div className="space-y-2">
+            <Label htmlFor="image">Image</Label>
+            <Input type="file" id="image" name="image" required/>
+        </div>
+        <Button type="submit">Save</Button>
+    </form>
+}
